cc9d365110ed1c78a27aa649f35bcee28df8a56a
r-stl <r-stl@gmail.com>
2018-07-23 22:25:03 +0200

    added comment

--- metrics.py
+++ metrics.py
@@ -1,6 +1,9 @@
 import math
 
 def euclidean_distance(vector1, vector2):
-    dist = [(a - b)**2 for a, b in zip(vector1, vector2)]
-    dist = math.sqrt(sum(dist))
-    return dist
+    """ computes the euclidean distance between
+    the vectors a and b
+    """
+    distance = [(a - b)**2 for a, b in zip(vector1, vector2)]
+    distance = math.sqrt(sum(distance))
+    return distance
